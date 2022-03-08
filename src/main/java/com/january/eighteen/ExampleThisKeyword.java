99a5c48534a0f9347e0b41ff469c1e7016c26c46
krunal <krunalpatel1210@gmail.com>
2022-03-08 13:31:59 -0500

    update

---
+++ src/main/java/com/january/eighteen/ExampleThisKeyword.java
@@ -0,0 +1,27 @@
+package com.january.eighteen;
+class Details
+{
+	String name;
+	int id;
+	int pay;
+	Details(String name, int id,int pay)
+	{// if we remove this keyword then the instance variable and the parameter of contractor will same and it's create ambiguity.
+		//name = name;
+		this.name = name;
+		//id =id;
+		this.id = id;
+		//pay = pay;
+		this.pay = pay;
+	}
+	void display()
+	{
+		System.out.println("Name = "+name+" ID = "+id+" Pay = "+pay);
+	}
+}
+public class ExampleThisKeyword {
+	public static void main (String args[])
+	{
+		Details d = new Details("Krunal",007,1500);
+		d.display();
+	}
+}
