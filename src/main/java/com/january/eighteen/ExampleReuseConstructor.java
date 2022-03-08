f3e6d40711bf1383ce0968892da93b278cb17f4b
krunal <krunalpatel1210@gmail.com>
2022-03-08 14:31:05 -0500

    update

---
+++ src/main/java/com/january/eighteen/ExampleReuseConstructor.java
@@ -0,0 +1,31 @@
+package com.january.eighteen;
+class FullDetails
+{
+	int id;
+	String name,course;
+	float fee;
+	FullDetails(int id,String name,String course)
+	{
+		this.id = id;
+		this.name = name;
+		this.course = course;
+	}
+	FullDetails(int id,String name,String course,float fee)
+	{
+		this(id,name,course);//we can reuse the constructor using this keyword, but the ()this must be the first statement in the constructor.  
+		this.fee =fee;
+	}
+	void display()
+	{
+		System.out.println("User ID = "+id+" Name = "+name+" Course = "+course+" Fee = "+fee);
+	}
+}
+public class ExampleReuseConstructor {
+	public static void main(String args[])
+	{
+		FullDetails d = new FullDetails(007,"Krunal","MCA");
+		FullDetails d1 = new FullDetails(011,"Vishal","MCA",10000);
+		d.display();
+		d1.display();
+	}
+}
